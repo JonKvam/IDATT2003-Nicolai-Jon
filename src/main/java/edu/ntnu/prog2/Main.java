7a54e8a65186dddec7a7865f10384526b964fc45
JonKvam <kvammen73@gmail.com>
2026-02-05 13:18:33 +0100

    Changed name of file path to make package work, couldn't handle "-"

--- src/main/java/edu/ntnu/prog2/Main.java
+++ src/main/java/edu/ntnu/prog2/Main.java
@@ -1,4 +1,4 @@
-package edu.ntnu.prog2-n-j;
+package edu.ntnu.prog2;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
