1ae2d650e1b5e932b6d47860874d6086ae48721d
Nicolai <your@gmail.com>
2026-02-13 14:39:34 +0100

    Created the Share class and the Stock class

---
+++ src/main/java/edu/ntnu/prog2/Stock.java
@@ -0,0 +1,39 @@
+package edu.ntnu.prog2;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stock {
+    private final String symbol;
+    private final String company;
+    private final List<BigDecimal> prices;
+
+    public Stock(final String symbol, final String company, final BigDecimal salesPrice) {
+        this.symbol = symbol;
+        this.company = company;
+        this.prices = new ArrayList<>();
+        this.prices.add(salesPrice);
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public BigDecimal getSalesPrice() {
+        if (prices.isEmpty()) {
+            return BigDecimal.ZERO;
+        }
+        return prices.getLast();
+    }
+
+    public void addNewSalesPrice(BigDecimal price) {
+        if (price != null) {
+            prices.add(price);
+        }
+    }
+}
