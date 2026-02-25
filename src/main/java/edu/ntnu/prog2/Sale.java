faec6c650dafde3bf311b0433f372cfbadbe4fee
Nicolai <your@gmail.com>
2026-02-25 10:59:11 +0100

    created the Sale class and fixed some minor things in the purchase class

--- src/main/java/edu/ntnu/prog2/Sale.java
+++ src/main/java/edu/ntnu/prog2/Sale.java
@@ -1,13 +1,13 @@
 package edu.ntnu.prog2;
 
-public class Purchase extends Transaction{
-    public Purchase(Share share, int week){
+public class Sale extends Transaction {
+    public Sale(Share share, int week) {
         super(share, week, new PurchaseCalculator(share));
     }
 
     @Override
     public void commit(Player player) {
-        if (isCommitted()) {
+        if (isCommitted()){
             throw new IllegalStateException("Transaction already committed");
         }
         setCommitted(true);
