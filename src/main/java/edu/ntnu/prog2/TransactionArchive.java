d5f8c1a08d9a866481af83ea9b4f18c1731ebe07
Nicolai <your@gmail.com>
2026-02-26 10:21:29 +0100

    created the testfile for the Stock class

--- src/main/java/edu/ntnu/prog2/TransactionArchive.java
+++ src/main/java/edu/ntnu/prog2/TransactionArchive.java
@@ -1,6 +1,7 @@
 package edu.ntnu.prog2;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class TransactionArchive {
 
@@ -17,7 +18,6 @@ public class TransactionArchive {
   public boolean isEmpty() {
     return transactions.isEmpty();
   }
-
   public List<Transaction> getTransactions(int week) {
     //TODO
   }
---
+++ src/test/java/edu/ntnu/prog2/StockTest.java
@@ -0,0 +1,40 @@
+package edu.ntnu.prog2;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.math.BigDecimal;
+
+class StockTest {
+    private Stock stock;
+    private final String SYMBOL = "NVDA";
+    private final String COMPANY = "Nvidia";
+    private final BigDecimal PRICE = new BigDecimal("150");
+
+    @BeforeEach
+    void setUp() {
+        stock = new Stock(SYMBOL, COMPANY, PRICE);
+    }
+
+    @Test
+    void testGetSymbol() {
+        assertEquals(SYMBOL, stock.getSymbol());
+    }
+
+    @Test
+    void testGetCompany() {
+        assertEquals(COMPANY, stock.getCompany());
+    }
+
+    @Test
+    void testGetSalesPrice() {
+        assertEquals(PRICE, stock.getSalesPrice());
+    }
+
+    @Test
+    void testAddNewSalesPrice() {
+        stock.addNewSalesPrice(new BigDecimal("170"));
+        assertEquals(new BigDecimal("170"), stock.getSalesPrice());
+    }
+}
