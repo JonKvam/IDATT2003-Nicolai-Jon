9bc863d41e84369e639a0de46f9ee5aadd78f429
JonKvam <kvammen73@gmail.com>
2026-03-25 11:13:03 +0100

    Started writing some Javadoc for PurchaseCalculator.

--- src/main/java/edu/ntnu/prog2/calculator/PurchaseCalculator.java
+++ src/main/java/edu/ntnu/prog2/calculator/PurchaseCalculator.java
@@ -1,13 +1,26 @@
 package edu.ntnu.prog2.calculator;
 
 import edu.ntnu.prog2.model.Share;
-
 import java.math.BigDecimal;
 
+/**
+ *Subclass for purchase calculator.
+ *
+ *<p>This class implements transaction calculator and consists of a constructor and
+ * different methods to calculate different costs regarding purchases </p>
+ *
+ * @author Nicolai
+ */
 public class PurchaseCalculator implements TransactionCalculator {
+
   private final BigDecimal purchasePrice;
   private final BigDecimal quantity;
 
+  /**
+   * Constructor for purchase calculator.
+   *
+   * @param share an instance of a share
+   */
   public PurchaseCalculator(Share share) {
     this.purchasePrice = share.getPurchasePrice();
     this.quantity = share.getQuantity();
