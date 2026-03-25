2b88d552b91abbcc90d684b3a58e526965e248be
JonKvam <kvammen73@gmail.com>
2026-03-25 12:28:17 +0100

    Added Javadoc for Player class.

--- src/main/java/edu/ntnu/prog2/model/Player.java
+++ src/main/java/edu/ntnu/prog2/model/Player.java
@@ -1,9 +1,15 @@
 package edu.ntnu.prog2.model;
 
 import edu.ntnu.prog2.service.TransactionArchive;
-
 import java.math.BigDecimal;
 
+/**
+ * Class for Player.
+ *
+ * <p>The class represents a player of the game. The class
+ * has a constructor and methods to keep track of the players
+ * actions, shares and balance. </p>
+ */
 public class Player {
 
   private final String name;
@@ -12,6 +18,13 @@ public class Player {
   private final Portfolio portfolio;
   private final TransactionArchive transactionArchive;
 
+  /**
+   * Constructor for Player.
+   *
+   * @param name any String to represent name of player
+   * @param startingMoney amount of money as BigDecimal representing money player begins with
+   * @param money BigDecimal to represent ongoing balance of player
+   */
   public Player(String name, BigDecimal startingMoney, BigDecimal money) {
     if (name == null) {
       throw new IllegalArgumentException("name cannot be null");
@@ -29,28 +42,60 @@ public class Player {
     transactionArchive = new TransactionArchive();
   }
 
+  /**
+   *get() method for name.
+   *
+   * @return name of player as a String
+   */
   public String getName() {
     return name;
   }
 
+  /**
+   *get() method for money.
+   *
+   * @return balance of player as BigDecimal
+   *
+   *
+   */
   public BigDecimal getMoney() {
     return money;
   }
 
+  /**
+   * Method for adding money to a players account.
+   *
+   * @param amount amount to be added to players balance
+   */
   public void addMoney(BigDecimal amount) {
     if(money == null) return;
     money = money.add(amount);
   }
 
+  /**
+   * Method for removing money from a players account.
+   *
+   * @param amount amount to be removed from a players balance
+   */
   public void withdrawMoney(BigDecimal amount) {
     if(money == null) return;
     money = money.subtract(amount);
   }
 
+  /**
+   * Method for accessing players portfolio.
+   *
+   * @return Portfolio as List over players shares.
+   */
   public Portfolio getPortfolio() {
     return portfolio;
   }
 
+  /**
+   * Method for accessing players transactions.
+   *
+   * @return transaction archive as List containing all transactions
+   */
   public TransactionArchive getTransactionArchive() {
     return transactionArchive;
   }
