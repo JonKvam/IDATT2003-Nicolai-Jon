59f3c6ddf2e6c021d7e285410057bde723c1e884
JonKvam <kvammen73@gmail.com>
2026-05-12 12:18:05 +0200

    Added Javadoc to SaleCalculator class.

--- src/main/java/edu/ntnu/prog2/calculator/SaleCalculator.java
+++ src/main/java/edu/ntnu/prog2/calculator/SaleCalculator.java
@@ -4,28 +4,56 @@ import edu.ntnu.prog2.model.Share;
 
 import java.math.BigDecimal;
 
+/**
+ *Subclass for sale calculator.
+ *
+ *<p>This class implements transaction calculator and consists of a constructor and
+ * different methods to calculate different costs regarding sales </p>
+ *
+ * @author Nicolai
+ */
 public class SaleCalculator implements TransactionCalculator {
   private final BigDecimal purchasePrice;
   private final BigDecimal salesPrice;
   private final BigDecimal quantity;
 
+  /**
+   * Constructor for sale calculator.
+   *
+   * @param share an instance of a share
+   */
   public SaleCalculator(Share share) {
     this.purchasePrice = share.getPurchasePrice();
     this.salesPrice = share.getStock().getSalesPrice();
     this.quantity = share.getQuantity();
   }
 
+  /**
+   * Method for calculating gross value on sales.
+   *
+   * @return gross value as BigDecimal
+   */
   @Override
   public BigDecimal calculateGross() {
     return salesPrice.multiply(quantity);
   }
 
+  /**
+   * Method for calculating tax on sales.
+   *
+   * @return commission as BigDecimal.
+   */
   @Override
   public BigDecimal calculateCommission() {
     BigDecimal commissionRate = new BigDecimal("0.01");
     return calculateGross().multiply(commissionRate);
   }
 
+  /**
+   * Method for calculating tax on sales.
+   *
+   * @return tax as BigDecimal
+   */
   @Override
   public BigDecimal calculateTax() {
     BigDecimal gross = calculateGross();
@@ -37,6 +65,11 @@ public class SaleCalculator implements TransactionCalculator {
     return profit.multiply(taxRate);
   }
 
+  /**
+   * Method for calculating total value of sales.
+   *
+   * @return Total sale value as BigDecimal
+   */
   @Override
   public BigDecimal calculateTotal() {
     return calculateGross().subtract(calculateCommission()).subtract(calculateTax());
