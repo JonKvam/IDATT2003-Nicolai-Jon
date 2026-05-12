d91601cc8156d1cf59e48c71b778dfe3e1ac064f
JonKvam <kvammen73@gmail.com>
2026-05-12 14:25:05 +0200

    Added Javadoc to Share class.

--- src/main/java/edu/ntnu/prog2/model/Share.java
+++ src/main/java/edu/ntnu/prog2/model/Share.java
@@ -2,25 +2,56 @@ package edu.ntnu.prog2.model;
 
 import java.math.BigDecimal;
 
+/**
+ * Class for share.
+ *
+ * <p>The class a share. The share consists of a number of
+ * stocks resulting in a value based on the amount of stocks. The class
+ * consists of a constructor and some get-methods to access information. </p>
+ *
+ * @author Nicolai
+ */
 public class Share {
   private final Stock stock;
   private final BigDecimal quantity;
   private final BigDecimal purchasePrice;
 
-    public Share(Stock stock, BigDecimal quantity, BigDecimal purchasePrice) {
-        this.stock = stock;
-        this.quantity = quantity;
-        this.purchasePrice = purchasePrice;
-    }
+  /**
+   * Constructor for share.
+   *
+   * @param stock part of a company to be traded
+   * @param quantity amount of stocks to be traded
+   * @param purchasePrice value of stock
+   */
+  public Share(Stock stock, BigDecimal quantity, BigDecimal purchasePrice) {
+      this.stock = stock;
+      this.quantity = quantity;
+      this.purchasePrice = purchasePrice;
+  }
 
+  /**
+   * Get-method for stocks.
+   *
+   * @return stock as stock.
+   */
   public Stock getStock() {
     return stock;
   }
 
+  /**
+   * Get-method for amount of stocks.
+   *
+   * @return amount of stocks as BigDecimal
+   */
   public BigDecimal getQuantity() {
     return quantity;
   }
 
+  /**
+   * Get-method for value of stock.
+   *
+   * @return value of stock as BigDecimal
+   */
   public BigDecimal getPurchasePrice() {
     return purchasePrice;
   }
