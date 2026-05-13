7d88de7301fc81dab9afc15c369818251255b0a7
JonKvam <kvammen73@gmail.com>
2026-05-13 12:43:35 +0200

    Written similar Javadoc for both subclasses of Transaction (Purchase and Sale).

--- src/main/java/edu/ntnu/prog2/service/Sale.java
+++ src/main/java/edu/ntnu/prog2/service/Sale.java
@@ -5,14 +5,33 @@ import edu.ntnu.prog2.model.Player;
 import edu.ntnu.prog2.model.Share;
 import edu.ntnu.prog2.model.Transaction;
 
+/**
+ * Subclass extending Transaction to handle sales.
+ *
+ * <p>The class commits sales. </p>
+ *
+ * @author Nicolai
+ */
 public class Sale extends Transaction {
-    public Sale(Share share, int week) {
-        super(share, week, new PurchaseCalculator(share));
-    }
 
-    @Override
-    public void commit(Player player) {
-        validateCommit(player);
-        setCommitted(true);
-    }
+  /**
+   * Constructor for sales.
+   *
+   * @param share share to be sold
+   * @param week week when sale takes place
+   */
+  public Sale(Share share, int week) {
+    super(share, week, new PurchaseCalculator(share));
+  }
+
+  /**
+   * Validates and commits a sale.
+   *
+   * @param player player to commit sale
+   */
+  @Override
+  public void commit(Player player) {
+    validateCommit(player);
+    setCommitted(true);
+  }
 }
