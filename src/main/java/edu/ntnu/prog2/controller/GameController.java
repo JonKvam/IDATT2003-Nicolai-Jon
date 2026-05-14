6d126b1677f0d6eb8d74d6868a06b1da3130c5ae
JonKvam <kvammen73@gmail.com>
2026-05-14 14:57:34 +0200

    Javadoc for GameController.

--- src/main/java/edu/ntnu/prog2/controller/GameController.java
+++ src/main/java/edu/ntnu/prog2/controller/GameController.java
@@ -4,18 +4,37 @@ import edu.ntnu.prog2.model.Player;
 import edu.ntnu.prog2.model.Share;
 import edu.ntnu.prog2.model.Transaction;
 import edu.ntnu.prog2.service.Exchange;
-
 import java.math.BigDecimal;
 
+/**
+ * Controls possible actions for player.
+ *
+ * <p>Consists of a constructor and methods to make actions such as transactions
+ * and proceeding to next week.</p>
+ *
+ * @author Nicolai
+ */
 public class GameController {
   private final Player player;
   private final Exchange exchange;
 
+  /**
+   * Constructor for player and exchange.
+   *
+   * @param player player to commit actions
+   * @param exchange exchange for which actions occur
+   */
   public GameController(Player player, Exchange exchange) {
     this.player = player;
     this.exchange = exchange;
   }
 
+  /**
+   * Method to buy share as player.
+   *
+   * @param symbol symbol of stock/share
+   * @param quantity amount of stocks to be bought
+   */
   public void buyStock(String symbol, BigDecimal quantity) {
     try {
       Transaction transaction = exchange.buy(symbol, quantity, player);
@@ -25,6 +44,11 @@ public class GameController {
     }
   }
 
+  /**
+   * Method to sell share as player.
+   *
+   * @param share share to be sold
+   */
   public void sellShare(Share share) {
     try {
       Transaction transaction = exchange.sell(share, player);
@@ -34,6 +58,9 @@ public class GameController {
     }
   }
 
+  /**
+   * Method to advance to next week.
+   */
   public void nextWeek() {
     exchange.advance();
   }
