9a35f5a5c61a4451f7aa577532b9ea20ee4c388a
JonKvam <kvammen73@gmail.com>
2026-05-14 14:52:14 +0200

    Javadoc for TransactionFactory.

--- src/main/java/edu/ntnu/prog2/factory/TransactionFactory.java
+++ src/main/java/edu/ntnu/prog2/factory/TransactionFactory.java
@@ -5,13 +5,32 @@ import edu.ntnu.prog2.model.Transaction;
 import edu.ntnu.prog2.service.Purchase;
 import edu.ntnu.prog2.service.Sale;
 
+/**
+ * Factory class to create transactions.
+ *
+ * <p>Creates purchases and sales based on type of transaction.</p>
+ *
+ * @author Nicolai
+ */
 public class TransactionFactory {
 
+  /**
+   * Representation of type of transaction.
+   */
   public enum TransactionType {
     PURCHASE,
     SALE
   }
 
+  /**
+   * Creates transaction based on transaction type.
+   *
+   * @param type type of transaction to be created
+   * @param share share to be sold or purchased
+   * @param week week when transaction takes place
+   * @return purchase or sale based on type
+   * @throws IllegalArgumentException if transaction type is not sale or purchase
+   */
   public static Transaction createTransaction(TransactionType type, Share share, int week) {
     switch (type) {
       case PURCHASE:
